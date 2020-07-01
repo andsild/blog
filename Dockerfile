865f8d53a6607d1532da6ecd80d79013b778033c
Anders Sildnes <andsild@posteo.net>
2020-07-01 22:41:15 +0200

    The UNIX philosophy basically involves giving you enough rope to hang yourself.  And then a couple of feet more, just to be sure.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM ubuntu:18.04 as build
 RUN apt-get update && apt-get install -y \
   software-properties-common \
   libghc-zlib-dev
@@ -7,13 +7,12 @@ RUN add-apt-repository ppa:hvr/ghcjs
 RUN add-apt-repository ppa:hvr/ghc
 RUN apt-get update && apt-get install -y ghcjs-8.4 ghc-8.4.4 cabal-install-3.2
 ENV PATH="/opt/ghc/bin:/opt/ghcjs/bin:${PATH}"
-
-RUN cabal update
-RUN cabal new-install hakyll
+RUN cabal update && cabal new-install hakyll 
 
 COPY my-site /my-site
 WORKDIR /my-site
 RUN cabal build
 
+FROM build as runner
 # Hakyll's default port is 127.0.0.1, but for exposure through docker we need 0.0.0.0
-CMD ["cabal", "run", "my-site", "watch", "--", "--host", "0.0.0.0"]
+ENTRYPOINT ["cabal", "run", "my-site", "watch", "--", "--host", "0.0.0.0"]
---
+++ build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+  `maven-publish`
+}
+
+version = "0.0.1"
+description = """
+  bloooooog
+"""
+
+task<Exec>("builder") {
+  description = "build blog"
+  buildDir = File("./my-site/")
+  workingDir = File("my-site")
+  outputs.cacheIf { true }
+  commandLine("cabal", "build")
+  inputs.files(listOf(fileTree("*.hs"), fileTree("*.cabal")))
+    .withPropertyName("sourceFiles")
+    .withPathSensitivity(PathSensitivity.RELATIVE)
+  outputs.dir("./my-site/dist-newstyle/build/")
+}
+
+task<Exec>("installer") {
+  dependsOn("builder")
+  outputs.cacheIf { true }
+  description = "install stuff"
+  workingDir = File("my-site")
+  commandLine("cabal", "install", "--program-suffix=.bin", ".", "--installdir=target", "--overwrite-policy=always")
+  inputs.files(listOf(fileTree("./my-site/image/*"), fileTree("./my-site/*.markdown"), fileTree("my-site/**/.markdown")))
+    .withPropertyName("sourceFiles")
+    .withPathSensitivity(PathSensitivity.RELATIVE)
+  outputs.dir("./my-site/dist-newstyle/target")
+}
+
+task<Tar>("zipper") {
+  dependsOn("installer")
+  compression = Compression.GZIP
+  from("./my-site") {
+    include(setOf("*.html", "*.markdown", "css/*", "images/*", "posts/*", "templates/*", "target/site.bin"))
+
+    filesMatching("target/site.bin") {
+      path = "blog/"
+      name = "blog/site.bin"
+      }
+  }
+  into("blog")
+  setArchiveName("blog.tar.gz")
+  setDestinationDir(File("./my-site/target/"))
+}
+
+val exec = file("./my-site/target/blog.tar.gz")
+val haskell by configurations.creating
+val artifact = artifacts.add("haskell", exec) {
+  type = "tarball"
+  builtBy("zipper")
+  classifier = "prod"
+  name = "blog"
+  extension = "tar.gz"
+}
+val repoUser: String by project
+val repoPassword: String by project
+
+publishing {
+  publications {
+    create<MavenPublication>("blog") {
+      groupId = "qwde.blog"
+      description = "Haskell binary to run isomorhpic HTTP frontend"
+      artifact(artifact)
+      artifactId = "blog"
+      pom {
+        name.set("blog")
+        description.set("Haskell binary to run blog")
+        url.set("http://qwde.no/blog")
+        developers {
+          developer {
+            id.set("andsild")
+            name.set("Anders")
+            email.set("trolo@lol.lol")
+          }
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      url = uri("http://qwde.no:8876/repository/internal/")
+      credentials {
+          // Store in ~/.gradle/gradle.properties
+          username = "admin"
+          password = repoPassword
+      }
+    }
+  }
+}
+
